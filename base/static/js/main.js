48edadba82368a7e0d8e0d00ff6a08c3e1624151
clincher <va.bolshakov@gmail.com>
2014-02-26 15:41:32 +0700

    comments tweaked

--- base/static/js/main.js
+++ base/static/js/main.js
@@ -1,6 +1,6 @@
 VK.init({apiId: 4202342, onlyWidgets: true});
 VK.Widgets.Recommended("vk_recommended", {limit: 3, period: 'month', verb: 1, width: "450"});
-VK.Widgets.Comments("vk_comments", {limit: 5, width: "450", attach: "*"});
+VK.Widgets.Comments("vk_comments", {limit: 10, width: "600", attach: "*"});
 VK.Widgets.Like("vk_like", {type: "full", height: 24});
 VK.Widgets.Group("vk_groups", {mode: 0, width: "285", height: "350", color1: 'ffffff', color2: '2B587A', color3: '333333'}, 58968036);
 
